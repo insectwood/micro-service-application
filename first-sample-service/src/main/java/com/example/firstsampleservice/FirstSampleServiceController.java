61640770a94f0b536597671b9de567881eb8647d
kwonc <jabykw@gmail.com>
2022-06-07 23:56:05 +0900

    add load balancer on apigateway

--- first-sample-service/src/main/java/com/example/firstsampleservice/FirstSampleServiceController.java
+++ first-sample-service/src/main/java/com/example/firstsampleservice/FirstSampleServiceController.java
@@ -1,16 +1,24 @@
 package com.example.firstsampleservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-sample-service")
 @Slf4j
 public class FirstSampleServiceController {
-
+    Environment env;
+    @Autowired
+    public FirstSampleServiceController(Environment env) {
+        this.env = env;
+    }
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to fisrt sample service";
@@ -23,7 +31,8 @@ public class FirstSampleServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "first service check check";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hello, this is a check from first sample service on Port %s", env.getProperty("local.server.port"));
     }
 }
