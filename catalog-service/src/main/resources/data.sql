cbc7b8918754bd7b057adf3975de9d86fce2b526
kwonc <jabykw@gmail.com>
2022-06-14 23:58:56 +0900

    make catalog service, change apigateway application

---
+++ catalog-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into msa.catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-001', 'shoose', 100, 3300);
+insert into msa.catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-002', 'AMD ryzen 5900x', 300, 59480);
+insert into msa.catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-003', 'figure', 25, 17600);
